4583082afa946f9830fb79c81180669b63fe9c18
Vaishali Bhardwaj <47493831+vaishalibhardwaj@users.noreply.github.com>
2024-08-04 12:36:20 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -6,15 +6,15 @@ export const dialogueData = {
     I've made a couple of projects in those languages.Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/vaishalibhardwaj" target="_blank">Github</a>!`,
   "cs-degree": `This is my ECE degree. I hung it on the wall because I'm proud of it. It was a very intensive degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch movies(mostly Iranian cinema these days) and browse reddit forums.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Japanese movies(mostly Kurosawa's movies) and browse reddit forums.`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
   <a href="https://www.youtube.com/@NeetCode" target="_blank">NeetCode</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1HGXxFrGaItPT5USZxcbckCBGOtvMjf7c/view" target="_blank">Check it out?</a>
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed and if I am not able to sleep I listen to bedtime stories in lifelike narration. Check it out on my <a href="https://github.com/vaishalibhardwaj/bedtimestories" target="_blank">Github</a>`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1yiqJureSwdI0-n-u3oHbnv2lY9Y7tSqA/view?usp=sharing" target="_blank">Check it out?</a>
   Contact me at vaishalibhardwaj5490@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about my projects are available on my github : <a href="https://github.com/vaishalibhardwaj" target="_blank">Github</a>.`,
-  library: `There are a lot of fiction books on my shelves. There is even one on programming.
+  library: `There are a lot of fiction books on my shelves. There is even one on learning Japanese.
   I probably only read one of them. Who else compulsively buys books without ever finishing them?`,
   exit: `If you want to exit vaishali's portfolio, just close the tab.`,
 };
